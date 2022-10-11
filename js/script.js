92bd41fa05b7751ef9954fa55c77303fac6c6de7
Zuhra Hashimi <zuhra.tactaz@yahoo.com>
2022-10-11 20:54:13 +0430

    solve

--- js/script.js
+++ js/script.js
@@ -5,13 +5,12 @@ const navSlide = () => {
     nav.classList.toggle('nav-active');
     burger.classList.toggle('toggle');
   });
-
   document.querySelectorAll('.nav-link').forEach((n) =>
-    n.addEventListener('click', () => {
+  n.addEventListener('click', () => {
       nav.classList.remove('nav-active');
       burger.classList.remove('toggle');
     })
   );
-};
+}
 
 navSlide();
