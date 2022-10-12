2e18170efba03b5a96ad32d80de0db7346f7d84c
Zuhra Hashimi <zuhra.tactaz@yahoo.com>
2022-10-12 20:20:58 +0430

    add popup content

--- js/script.js
+++ js/script.js
@@ -29,3 +29,24 @@ modalClose.addEventListener('click', () => {
   modalBg.style.visibility = 'hidden';
   modalBg.style.opacity = 0;
 });
+
+/* card-modal */
+
+const popupBtn = document.querySelector('.card-button');
+const popupTitle = [];
+document.querySelector('div.flex-container > h4').forEach((item) => {
+  popupTitle.push(item.textContent);
+});
+const popupTechno = ['HTML', 'Bootstrap', 'Ruby on Rails'];
+const popupDescrip = 'Budget-App is about building a mobile web application where you can manage your budget: you have a list of transactions associated with a category, so that you can see how much money you spent and on what';
+const popupLive = 'https://budget1-app.herokuapp.com/splashs/index/';
+const popupSource = 'https://github.com/ZuhRa-HashiMi/budget-app';
+const popupObj = {};
+for (let i=0; i < popupTitle.length; i += 1) {
+  popupObj[`Project${i}`] = {};
+  popupObj[`Project${i}`].title = popupTitle[i];
+  popupObj[`Project${i}`].technologies = popupTechno;
+  popupObj[`Project${i}`].description = popupDescrip;
+  popupObj[`Project${i}`].liveLink = popupLive;
+  popupObj[`Project${i}`].sourceLink = popupSource;
+}
